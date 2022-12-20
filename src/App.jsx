85c894908d0a75606dcade6efdfb425f1616dd84
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-20 17:27:47 +0530

    context implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StockOverview from "./pages/StockOverview";
 import StockDetails from "./pages/StockDetails";
+import { WatchListContextProvider } from "./context/WatchListContext";
 function App() {
-  return (
-    <main>
-      <Router>
-        <Routes>
-          <Route path="/" element={<StockOverview />} />
-          <Route path="/details/:stock" element={<StockDetails />} />
-        </Routes>
-      </Router>
-    </main>
-  );
+	return (
+		<main className="container">
+			<WatchListContextProvider>
+				<Router>
+					<Routes>
+						<Route path="/" element={<StockOverview />} />
+						<Route path="/detail/:symbol" element={<StockDetails />} />
+					</Routes>
+				</Router>
+			</WatchListContextProvider>
+		</main>
+	);
 }
 
 export default App;
