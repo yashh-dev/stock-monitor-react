97e5eadd9fc9995c0139fe8f0cd4b746f050edc6
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-20 17:28:25 +0530

    stock overview complete

--- src/pages/StockOverview.jsx
+++ src/pages/StockOverview.jsx
@@ -1,7 +1,12 @@
+import AutoComplete from "../components/AutoComplete";
+import StockList from "../components/StockList";
+
 const StockOverview = () => {
   return (
     <div>
       <h1>StockOverview</h1>
+      <AutoComplete />
+      <StockList />
     </div>
   );
 };
